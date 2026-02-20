0fdcfeb663c86234e44f8a8c01201201c9553118
Muhammad Farhan <j.farhan.muhammad@gmail.com>
2026-02-21 06:45:08 +0700

    feat(middleware): add request id middleware

---
+++ pkg/middleware/requestid.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"net/http"
+
+	wsctx "saas-ws-lib/pkg/ctx"
+
+	"github.com/google/uuid"
+)
+
+const HeaderRequestID = "X-Request-ID"
+
+func RequestID() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rid := r.Header.Get(HeaderRequestID)
+			if rid == "" {
+				rid = uuid.NewString()
+			}
+
+			w.Header().Set(HeaderRequestID, rid)
+
+			ctx := wsctx.WithRequestID(r.Context(), rid)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
