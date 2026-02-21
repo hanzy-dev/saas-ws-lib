99e8fded0ff4e555439bfcce0e96470a0bc3f88e
Muhammad Farhan <j.farhan.muhammad@gmail.com>
2026-02-21 18:19:20 +0700

    feat(config): add env config helpers

---
+++ pkg/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+func String(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
+
+func Bool(key string, def bool) bool {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		return def
+	}
+	return b
+}
+
+func Int(key string, def int) int {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	n, err := strconv.Atoi(v)
+	if err != nil {
+		return def
+	}
+	return n
+}
+
+func Duration(key string, def time.Duration) time.Duration {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		return def
+	}
+	return d
+}
