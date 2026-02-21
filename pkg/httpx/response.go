3b33c0f4d80794923788e22c5d39f6b9282af68b
Muhammad Farhan <j.farhan.muhammad@gmail.com>
2026-02-21 16:34:20 +0700

    feat(httpx): add json response helpers

---
+++ pkg/httpx/response.go
@@ -0,0 +1,16 @@
+package httpx
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(v)
+}
+
+func NoContent(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNoContent)
+}
