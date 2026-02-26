8e5dba6c6c4b613867be09525f0f60440c15f49c
Muhammad Farhan <j.farhan.muhammad@gmail.com>
2026-02-27 02:01:27 +0700

    middleware: document middleware chain

--- pkg/middleware/chain.go
+++ pkg/middleware/chain.go
@@ -2,8 +2,12 @@ package middleware
 
 import "net/http"
 
+// Middleware represents an HTTP middleware function.
 type Middleware func(http.Handler) http.Handler
 
+// Chain applies middlewares to handler.
+// Middlewares are applied in the order provided:
+// Chain(h, A, B) => A(B(h))
 func Chain(h http.Handler, mws ...Middleware) http.Handler {
 	for i := len(mws) - 1; i >= 0; i-- {
 		h = mws[i](h)
